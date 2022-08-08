be395c782984cba0fd374c7bb53173855c2fead7
crmontiel <crmontiel043@gmail.com>
2022-08-08 02:07:50 -0600

    add build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBwnNWmhTCURPSkwWvyO_v-3Md13gM-H1Y",
+    authDomain: "smartcapital-f2b8b.firebaseapp.com",
+    projectId: "smartcapital-f2b8b",
+    storageBucket: "smartcapital-f2b8b.appspot.com",
+    messagingSenderId: "959397387241",
+    appId: "1:959397387241:web:cb24fc283ab2c9396c0978"
+  }
 };
